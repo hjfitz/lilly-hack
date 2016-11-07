bdf03b4a9da94789410551b2c51dbd8fa67ed44a
Harry Fitzgerald <hfitz83@gmail.com>
2016-11-07 18:04:36 +0000

    refined and tested schema. she lives!

--- schema.sql
+++ schema.sql
@@ -1,7 +1,12 @@
 -- Create user TABLE
 -- password could be CHAR() depending on hashing also used
+
+-- connect to database!
+-- source ~/.profile
+-- heroku pg:psql --app lilly-hack-hjf
+
 CREATE TABLE USERS (
-  user_id      SERIAL         NOT NULL PRIMARY KEY,
+  user_id      SERIAL      NOT NULL PRIMARY KEY,
   user_name    VARCHAR(30) NOT NULL,
   user_pass    VARCHAR(30) NOT NULL,
   user_created DATE        NOT NULL,
@@ -12,7 +17,7 @@ CREATE TABLE USERS (
 -- Because color codes are fixed length, colors are all 6 in length.
 -- One user may have many 'outfits'
 CREATE TABLE PREFERENCES (
-  pref_id    INT     NOT NULL PRIMARY KEY,
+  pref_id    SERIAL     NOT NULL PRIMARY KEY,
   user_id    INT     NOT NULL REFERENCES USERS(user_id),
   skinCol    CHAR(6) NOT NULL,
   hairCol    CHAR(6) NOT NULL,
@@ -24,8 +29,18 @@ CREATE TABLE PREFERENCES (
 
 -- one user has many todos
 CREATE TABLE TODO (
-  todo_id    INT NOT NULL PRIMARY KEY,
+  todo_id    SERIAL NOT NULL PRIMARY KEY,
   user_id    INT NOT NULL REFERENCES USERS(user_id),
   todo_title VARCHAR(30) NOT NULL,
   todo_text  VARCHAR(255)
 );
+
+-- general insert should be structured as follows in the db.js file (yet to be created)
+-- btw, double quotes makes postgres shit the based
+INSERT INTO USERS (user_name, user_pass, user_created, user_health, user_exp)
+  VALUES ('harry','god','1995-12-08',60,60);
+-- where the data is yyyy-mm-dd (datatype DATE must be in single quotes)
+INSERT INTO TODO (user_id, todo_title,todo_text)
+  VALUES (1,'test','test task');
+INSERT INTO PREFERENCES (user_id, skinCol, hairCol, teeCol, trouserCol, eyeCol, shoeCol)
+  VALUES (1, "FF00FF", "000000", "0000FF", "00FF00", "FF0000", "00FFFF");
